a31b6bc150e01dce9d87e60a9a579a60fbc845c2
Kodar11 <tanmay.chavan@walchandsangli.ac.in>
2024-08-03 12:58:41 +0530

    writing controllers

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -51,4 +51,41 @@ const userSchema = new Schema(
     }
 )
 
+userSchema.pre("save", async function (next) {
+    if(!(this.isModified("password"))) return next ();
+
+    this.password = bcrypt.hash(this.password,10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password,this.password)
+}
+
+userSchema.method.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id : this._id,
+            username : this.username,
+            email : this.email
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expireIn : process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.method.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id : this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expireIn : process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
 export const user = mongoose.model("User",userSchema)
