bb88f61ce8e553991c9e64e27640d2497b09f0e3
Kodar11 <tanmay.chavan@walchandsangli.ac.in>
2024-08-03 19:35:18 +0530

    writing controllers for task

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -51,7 +51,7 @@ userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password,this.password)
 }
 
-userSchema.method.generateAccessToken = function(){
+userSchema.methods.generateAccessToken = function(){
     return jwt.sign(
         {
             _id : this._id,
@@ -60,21 +60,23 @@ userSchema.method.generateAccessToken = function(){
         },
         process.env.ACCESS_TOKEN_SECRET,
         {
-            expireIn : process.env.ACCESS_TOKEN_EXPIRY
+            expiresIn : process.env.ACCESS_TOKEN_EXPIRY
         }
-    )
+    );
 }
 
-userSchema.method.generateRefreshToken = function(){
+
+
+userSchema.methods.generateRefreshToken = function(){
     return jwt.sign(
         {
             _id : this._id
         },
         process.env.REFRESH_TOKEN_SECRET,
         {
-            expireIn : process.env.REFRESH_TOKEN_EXPIRY
+            expiresIn : process.env.REFRESH_TOKEN_EXPIRY
         }
-    )
+    );
 }
 
 export const User = mongoose.model("User",userSchema)
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,13 @@
 import Router from "express"
-import {registerUser} from "../controllers/user.controllers.js"
-import { upload } from "../middlewares/multer.middlewares.js"
+import { registerUser,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails } from "../controllers/user.controllers.js"
+import { verifyJWT } from '../middlewares/auth.middlewares.js';
+
 
 const router = Router()
 
@@ -13,8 +20,17 @@ const router = Router()
 //     ]),
     
 //    await registerUser)
-
-router.post('/register', upload.single('avatar'), registerUser);
-
+// router.post('/register', upload.single('avatar'), registerUser);
 // router.post('/register', registerUser);
+
+//secured routes
+router.route("/register").post(registerUser)
+router.route("/login").post(loginUser)
+router.route("/logout").post(verifyJWT,  logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+
 export default router
